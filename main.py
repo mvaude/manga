6212fdac47e0c33a2782b72a95ff4313e0df8705
Maxime Vaude <maxime.vaude@gmail.com>
2016-11-28 00:56:46 +0100

    ft(async): add event loop

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from urllib.request import urlopen
+import asyncio
 import os
 import pprint
 import re
 import sys
+import time
+from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 import requests
@@ -16,10 +18,11 @@ class Manga(object):
         self.get_chapters()
 
     def get_scan(self, url):
-        print("URL: {}".format(url))
+        #`print("URL: {}".format(url))
         # html = urlopen(url)
         html = requests.get(url, allow_redirects=False)
-        print(html.status_code)
+        html.close()
+        # print(html.status_code)
         if html.status_code == 200:
             bsObj = BeautifulSoup(html.text, "html.parser")
             res = bsObj.find_all("img", id="mainImg")
@@ -38,19 +41,20 @@ class Manga(object):
         if not os.path.exists(path):
             os.makedirs(path)
         with open("./{}/{:03d}.{}".format(path, page, self.get_ext(scan)), "wb") as f:
-            print("SCAN: ", scan)
+            # print("SCAN: ", scan)
             r = requests.get(scan)
-            print(r.status_code)
+            # print(r.status_code)
             f.write(r.content)
 
     def get_chapters(self):
         self.chapters = []
         r = requests.get("{}/{}/".format(self.base_url, self.name))
+        r.close()
         if r.status_code == 200:
             bsObj = BeautifulSoup(r.text, "html.parser")
             for scan in bsObj.find_all("a", "chapterLink"):
                 chapter = re.findall(r'/(\d+\.*\d*)/1/', scan.attrs["href"])
-                print(chapter)
+                #print(chapter)
                 if len(chapter[0]) > 0:
                     try:
                         num = int(chapter[0])
@@ -58,26 +62,49 @@ class Manga(object):
                         num = float(chapter[0])
                     self.chapters.append(num)
         self.chapters = sorted(self.chapters) 
-        print(self.chapters)
-    
-def main(manga):
-    mng = Manga(manga)
-    count = 0
-    for chapter in mng.chapters:
+        # print(self.chapters)
+
+    async def get_scans(self, url, chapter, page):
+        scan = self.get_scan(url)
+        if scan == "":
+            return
+        self.write_scan(scan, chapter, page)
+        return (chapter, page)
+
+async def get_routines(manga):
+    res = []
+    for chapter in manga.chapters:
         for page in range(1, 1000):
-            url = "{}{}/{}/{}/".format(mng.base_url, mng.name, chapter, page)
-            scan = mng.get_scan(url)
-            if scan == "":
-                count += 1
+            url = "{}{}/{}/{}/".format(manga.base_url, manga.name, chapter, page)
+            html = requests.get(url, allow_redirects=False)
+            html.close()
+            if html.status_code != 200:
                 break
-            mng.write_scan(scan, chapter, page)
-            count = 0
-        if count == 3:
-            break
+            res.append(manga.get_scans(url, chapter, page))
+    return res
+
+
+async def main(manga):
+    mng = Manga(manga)
+    start_time = time.time()
+    print("start list of routines")
+    routines = await get_routines(mng)
+    print("finish list of routines {}s".format(time.time() - start_time))
+    tmp_time = time.time()
+    print("start async.wait")
+    completed, pending = await asyncio.wait(routines)
+    print("finish async.wait {}s".format(time.time() - tmp_time))
+    res = []
+    for item in completed:
+        (chapter, page) = item.result()
+        res.append((chapter, page))
+    print("Total time: {}s".format(time.time() - start_time))
 
 if __name__ == "__main__":
     if (len(sys.argv) != 2):
         print("No enough arguments")
     else:
         print(sys.argv[1])
-        main(sys.argv[1])
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(sys.argv[1]))
+        loop.close()
